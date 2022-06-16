6026f921a02901791189edbd94e54cdd53baa16d
Taeuk Noh <taeuk4958@gmail.com>
2022-06-16 20:10:02 +0900

    modify get_palette for load classes from json

--- custom_mmdet/datasets/custom.py
+++ custom_mmdet/datasets/custom.py
@@ -54,12 +54,9 @@ class CustomDataset(Dataset):
             during tests.
     """
 
-    CLASSES = ('flower', 'midrid', 'leaf', 'stem', 'fruit', 'cap')      # dataset.json의 categoris의 순서대로 입력해야 한다.
-
-    # just for visualization. no relationship with classification loss. 
-    PALETTE = [(220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230),
-               (106, 0, 228), (0, 60, 100)]
-
+    CLASSES = None    
+        
+    PALETTE = None
 
     def __init__(self,
                  ann_file,
@@ -80,8 +77,9 @@ class CustomDataset(Dataset):
         self.test_mode = test_mode
         self.filter_empty_gt = filter_empty_gt
         self.file_client = mmcv.FileClient(**file_client_args)
-        self.CLASSES = self.get_classes(classes)
-
+        self.CLASSES = self.get_classes(img_prefix, classes)
+        self.PALETTE = self.get_palette()
+    
         # print(f"self.ann_file : {self.ann_file}")
         # print(f"self.data_root : {self.data_root}")
         # print(f"self.img_prefix : {self.img_prefix}")
@@ -92,40 +90,6 @@ class CustomDataset(Dataset):
         # print(f"self.CLASSES : {self.CLASSES}")
         # exit()
 
-        
-        
-        # file_list = os.listdir(img_prefix) 
-        # for file_name in file_list:
-        #     if file_name.split("_")[-1] == 'claasses.json':
-        #         classes =osp.json(img_prefix, file_name)
-        # CLASSES = classes
-        
-        # palette = [(220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230),
-        #        (106, 0, 228), (0, 60, 100), (0, 80, 100), (0, 0, 70),
-        #        (0, 0, 192), (250, 170, 30), (100, 170, 30), (220, 220, 0),
-        #        (175, 116, 175), (250, 0, 30), (165, 42, 42), (255, 77, 255),
-        #        (0, 226, 252), (182, 182, 255), (0, 82, 0), (120, 166, 157),
-        #        (110, 76, 0), (174, 57, 255), (199, 100, 0), (72, 0, 118),
-        #        (255, 179, 240), (0, 125, 92), (209, 0, 151), (188, 208, 182),
-        #        (0, 220, 176), (255, 99, 164), (92, 0, 73), (133, 129, 255),
-        #        (78, 180, 255), (0, 228, 0), (174, 255, 243), (45, 89, 255),
-        #        (134, 134, 103), (145, 148, 174), (255, 208, 186),
-        #        (197, 226, 255), (171, 134, 1), (109, 63, 54), (207, 138, 255),
-        #        (151, 0, 95), (9, 80, 61), (84, 105, 51), (74, 65, 105),
-        #        (166, 196, 102), (208, 195, 210), (255, 109, 65), (0, 143, 149),
-        #        (179, 0, 194), (209, 99, 106), (5, 121, 0), (227, 255, 205),
-        #        (147, 186, 208), (153, 69, 1), (3, 95, 161), (163, 255, 0),
-        #        (119, 0, 170), (0, 182, 199), (0, 165, 120), (183, 130, 88),
-        #        (95, 32, 0), (130, 114, 135), (110, 129, 133), (166, 74, 118),
-        #        (219, 142, 185), (79, 210, 114), (178, 90, 62), (65, 70, 15),
-        #        (127, 167, 115), (59, 105, 106), (142, 108, 45), (196, 172, 0),
-        #        (95, 54, 80), (128, 76, 255), (201, 57, 1), (246, 0, 122),
-        #        (191, 162, 208)]
-        
-        # PALETTE = []
-        # for i in range(len(palette)):
-        #     PALETTE.append(palette[i])
-        
         
         # join paths if data_root is specified
         if self.data_root is not None:
@@ -187,6 +151,39 @@ class CustomDataset(Dataset):
         """Total number of samples of data."""
         return len(self.data_infos)
 
+
+    def get_palette(self):
+        if self.CLASSES is not None:
+            # just for visualization. no relationship with classification loss.
+            palette = [(220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230),
+                    (106, 0, 228), (0, 60, 100), (0, 80, 100), (0, 0, 70),
+                    (0, 0, 192), (250, 170, 30), (100, 170, 30), (220, 220, 0),
+                    (175, 116, 175), (250, 0, 30), (165, 42, 42), (255, 77, 255),
+                    (0, 226, 252), (182, 182, 255), (0, 82, 0), (120, 166, 157),
+                    (110, 76, 0), (174, 57, 255), (199, 100, 0), (72, 0, 118),
+                    (255, 179, 240), (0, 125, 92), (209, 0, 151), (188, 208, 182),
+                    (0, 220, 176), (255, 99, 164), (92, 0, 73), (133, 129, 255),
+                    (78, 180, 255), (0, 228, 0), (174, 255, 243), (45, 89, 255),
+                    (134, 134, 103), (145, 148, 174), (255, 208, 186),
+                    (197, 226, 255), (171, 134, 1), (109, 63, 54), (207, 138, 255),
+                    (151, 0, 95), (9, 80, 61), (84, 105, 51), (74, 65, 105),
+                    (166, 196, 102), (208, 195, 210), (255, 109, 65), (0, 143, 149),
+                    (179, 0, 194), (209, 99, 106), (5, 121, 0), (227, 255, 205),
+                    (147, 186, 208), (153, 69, 1), (3, 95, 161), (163, 255, 0),
+                    (119, 0, 170), (0, 182, 199), (0, 165, 120), (183, 130, 88),
+                    (95, 32, 0), (130, 114, 135), (110, 129, 133), (166, 74, 118),
+                    (219, 142, 185), (79, 210, 114), (178, 90, 62), (65, 70, 15),
+                    (127, 167, 115), (59, 105, 106), (142, 108, 45), (196, 172, 0),
+                    (95, 54, 80), (128, 76, 255), (201, 57, 1), (246, 0, 122),
+                    (191, 162, 208)]
+            
+            tmp_palette = []
+            for i in range(len(self.CLASSES)):
+                tmp_palette.append(palette[i])
+            return tmp_palette
+        return None
+    
+    
     def load_annotations(self, ann_file):
         """Load annotation from COCO style annotation file.
 
@@ -435,7 +432,7 @@ class CustomDataset(Dataset):
         return self.pipeline(results)
 
     @classmethod
-    def get_classes(cls, classes=None):
+    def get_classes(cls, img_prefix, classes=None):
         """Get class names of current dataset.
 
         Args:
@@ -449,7 +446,17 @@ class CustomDataset(Dataset):
             tuple[str] or list[str]: Names of categories of the dataset.
         """
         if classes is None:
-            return cls.CLASSES
+            classes_json = None
+            file_list = os.listdir(img_prefix) 
+            for file_name in file_list:
+         
+                if file_name.split("_")[-1] == 'classes.json':
+                    classes_json =osp.join(img_prefix, file_name)
+            
+            if classes_json is not None:
+                classes = mmcv.load(classes_json)   
+                return classes             
+            else:   return cls.CLASSES
 
         if isinstance(classes, str):
             # take it as a file path
