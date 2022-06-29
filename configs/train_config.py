dbbf3bc5ca24ff39e1f5c6826e6c24a4aa030e0c
Taeuk Noh <taeuk4958@gmail.com>
2022-06-29 18:04:29 +0900

    add device for inference

--- configs/train_config.py
+++ configs/train_config.py
@@ -12,6 +12,7 @@ mode = 'train'
 output = 'result.pkl'
 show_dir = 'result_images'
 
+device = 'cuda:0'
 
 pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth'  # noqa
 model = dict(
