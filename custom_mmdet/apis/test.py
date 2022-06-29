72936ad84ba3c04d98437ca51a0afb6922a08f55
Taeuk Noh <taeuk4958@gmail.com>
2022-06-29 14:52:38 +0900

    modify argu for test code

--- custom_mmdet/apis/test.py
+++ custom_mmdet/apis/test.py
@@ -16,28 +16,30 @@ from custom_mmdet.core import encode_mask_results
 
 def single_gpu_test(model,
                     data_loader,
-                    show=False,
                     out_dir=None,
+                    img_dir=None,
                     show_score_thr=0.3):
     model.eval()
     results = []
     dataset = data_loader.dataset
     PALETTE = getattr(dataset, 'PALETTE', None)
-    prog_bar = mmcv.ProgressBar(len(dataset))
-    for i, data in enumerate(data_loader):
+    prog_bar = mmcv.ProgressBar(len(dataset))   
+    
+    for i, data in enumerate(data_loader):     
         with torch.no_grad():
             result = model(return_loss=False, rescale=True, **data)
-
         batch_size = len(result)
-        if show or out_dir:
+
+        if out_dir and img_dir:         
             if batch_size == 1 and isinstance(data['img'][0], torch.Tensor):
                 img_tensor = data['img'][0]
             else:
                 img_tensor = data['img'][0].data[0]
+            
             img_metas = data['img_metas'][0].data[0]
             imgs = tensor2imgs(img_tensor, **img_metas[0]['img_norm_cfg'])
             assert len(imgs) == len(img_metas)
-
+            
             for i, (img, img_meta) in enumerate(zip(imgs, img_metas)):
                 h, w, _ = img_meta['img_shape']
                 img_show = img[:h, :w, :]
@@ -49,14 +51,14 @@ def single_gpu_test(model,
                     out_file = osp.join(out_dir, img_meta['ori_filename'])
                 else:
                     out_file = None
-
+                          
                 model.module.show_result(
                     img_show,
                     result[i],
                     bbox_color=PALETTE,
                     text_color=PALETTE,
                     mask_color=PALETTE,
-                    show=show,
+                    show=True,
                     out_file=out_file,
                     score_thr=show_score_thr)
 
