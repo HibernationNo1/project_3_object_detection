2af4002e1a44436b388a746b823f7e0622a8754f
Taeuk Noh <taeuk4958@gmail.com>
2022-07-04 16:48:46 +0900

    add to registry about solov2 module

--- custom_mmdet/models/detectors/__init__.py
+++ custom_mmdet/models/detectors/__init__.py
@@ -43,6 +43,8 @@ from .yolo import YOLOV3
 from .yolof import YOLOF
 from .yolox import YOLOX
 
+from .solov2 import SOLOv2
+
 __all__ = [
     'ATSS', 'BaseDetector', 'SingleStageDetector', 'TwoStageDetector', 'RPN',
     'KnowledgeDistillationSingleStageDetector', 'FastRCNN', 'FasterRCNN',
@@ -52,5 +54,6 @@ __all__ = [
     'VFNet', 'DETR', 'TridentFasterRCNN', 'SparseRCNN', 'SCNet', 'SOLO',
     'DeformableDETR', 'AutoAssign', 'YOLOF', 'CenterNet', 'YOLOX',
     'TwoStagePanopticSegmentor', 'PanopticFPN', 'QueryInst', 'LAD', 'TOOD',
-    'MaskFormer', 'Mask2Former'
+    'MaskFormer', 'Mask2Former',
+    'SOLOv2'
 ]
