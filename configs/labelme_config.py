d0cbc563bd470fa560b86b486b69193ec6042b35
Taeuk Noh <taeuk4958@gmail.com>
2022-07-15 17:43:32 +0900

    delete unnecessary part

--- configs/labelme_config.py
+++ configs/labelme_config.py
@@ -15,12 +15,6 @@ options = dict(
     ratio_val = 0.01,
     save_gt_image = False,
     visul_gt_image_dir = 'gt_images',
-    augmentation = dict(
-        augmentation_flag = False,   # dataset자체적으로 augmentation진행할지 여부
-        resize_flag = False,            # resizing으로 augmentation
-        resizing_max_size = 1280,       # resizing을 수행할 때 긴 면의 size
-        vertivcal_flip_flag = False     # flip으로 augmentation
-    ),
     only_val_obj = False        # valid_objec에 포함되지 않는 라벨이 있을 때 무시하는 경우 False, Error 발생시키는 경우 True
 )
 
